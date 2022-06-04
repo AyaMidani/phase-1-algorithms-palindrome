b6cc04c84a04817fa27efa3734770e891599dff0
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-06-04 23:00:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+    for (let i = 0; i < word.length / 2; i++) {
+
+        // check if first and last string are same
+        if (word[i] !== word[word.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 /* 
   Add your pseudocode here
+
+  Declare a function that accepts string
+split the string into half
+iterate over each letter in the string:
+  if letter is not equal the letter in the second half:
+    return false
+return true
+
 */
 
 /*
   Add written explanation of your solution here
+  1. Declare a function that accepts one argument, a string.
+  2. for loop for checking if the word is Palindrome or not
+  3. split the word into two halfs and compare the letters if they match it is a Palindrome
 */
 
 // You can run `node index.js` to view these console logs
